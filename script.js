2f71d472c721e1b73e8e5b23683474f60298335d
Piotr Glejzer <glejzerpiotr@gmail.com>
2018-05-14 19:18:16 +0200

    some changes with RWD

--- script.js
+++ script.js
@@ -43,3 +43,41 @@ $(document).on('click', 'a[href^="#"]', function (event) {
         scrollTop: $($.attr(this, 'href')).offset().top
     }, 900);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    var menu = document.getElementById("menu"),
+        menuButton = document.getElementById('menu-button');
+    menuButton.addEventListener('click', function () {
+
+        if (menu.className == "nav__ul") {
+            menu.classList.add('nav__ul--responsive');
+            this.firstChild.alt = "hide menu";
+        } else {
+            menu.classList.add('nav__ul--hide');
+            setTimeout(function () {
+                menu.classList.remove('nav__ul--responsive');
+                menu.classList.remove('nav__ul--hide');
+            }, 600);
+            this.firstChild.alt = "show menu";
+        }
+    });
+
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    var nav = document.getElementById('nav');
+    window.addEventListener('scroll', function () {
+        var currentPosition = pageYOffset;
+        if (currentPosition > 10) {
+            nav.classList.add('nav__sticky');
+        } else {
+            nav.classList.remove('nav__sticky');
+        }
+    });
+});
+
+$(document).ready(function ($) {
+    $('.nav__link').on('click', function () {
+        $(".nav__button--toggle").trigger('click');
+    });
+});
